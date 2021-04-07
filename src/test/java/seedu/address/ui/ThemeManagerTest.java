6fd1edde27f25b1b3e8f963e1ba3510febfa00fb
lue97 <llue97@hotmail.com>
2021-04-07 12:00:06 +0800

    Remove static block in ThemeManagerTest

--- src/test/java/seedu/address/ui/ThemeManagerTest.java
+++ src/test/java/seedu/address/ui/ThemeManagerTest.java
@@ -6,12 +6,9 @@ import org.junit.jupiter.api.Test;
 
 public class ThemeManagerTest {
 
-    static {
-        ThemeManager.init();
-    }
-
     @Test
     public void themeManager_getTheme_success() {
+        ThemeManager.init();
         assertEquals(ThemeFactory.getDefaultTheme(), ThemeManager.getTheme());
     }
 
